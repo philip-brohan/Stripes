8e2bd243a01cafdd912e35a755ad7a670df7e91e
Philip Brohan <philip@brohan.org>
2020-01-22 16:50:18 +0000

    Added stripes for UKESM1-0-LL.

---
+++ CMIP6/UKESM1-0-LL/get_data_MO.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Get the CMIP6 historical monthly tas for UKESM1-0-LL
+# Use managecmip - MO only.
+
+# List the avaialble data with 
+# managecmip list -M cmip6 -e historical -t Amon -v tas -i MOHC -m UKESM1-0-LL
+# and get it with managecmip get.
+
+# Make the data available on $SCRATCH
+mkdir -p /scratch/hadpb/CMIP6/UKESM1-0-LL/Historical
+ln -s /project/champ/data/CMIP6/CMIP/MOHC/UKESM1-0-LL/historical/*/Amon/tas/gn/v*/* /scratch/hadpb/CMIP6/UKESM1-0-LL/Historical/
