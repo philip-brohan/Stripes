463820acceb68eaacaf8a20c73629369618ee7e4
Philip Brohan <philip@brohan.org>
2020-01-23 14:34:24 +0000

    Added HadGEM3-GC31-MM model & updated CMIP get data scripts.

--- CMIP6/UKESM1-0-LL/get_data_MO.sh
+++ CMIP6/UKESM1-0-LL/get_data_MO.sh
@@ -3,10 +3,12 @@
 # Get the CMIP6 historical monthly tas for UKESM1-0-LL
 # Use managecmip - MO only.
 
-# List the avaialble data with 
-# managecmip list -M cmip6 -e historical -t Amon -v tas -i MOHC -m UKESM1-0-LL
-# and get it with managecmip get.
+# Get the monthly data
+managecmip get -M cmip6 -e historical -t Amon -v tas -i MOHC -m UKESM1-0-LL
+# and the daly data
+managecmip get -M cmip6 -e historical -t day -v tas -i MOHC -m UKESM1-0-LL
 
 # Make the data available on $SCRATCH
 mkdir -p /scratch/hadpb/CMIP6/UKESM1-0-LL/Historical
 ln -s /project/champ/data/CMIP6/CMIP/MOHC/UKESM1-0-LL/historical/*/Amon/tas/gn/v*/* /scratch/hadpb/CMIP6/UKESM1-0-LL/Historical/
+ln -s /project/champ/data/CMIP6/CMIP/MOHC/UKESM1-0-LL/historical/*/day/tas/gn/v*/* /scratch/hadpb/CMIP6/UKESM1-0-LL/Historical/
