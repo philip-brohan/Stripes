606bc118b5051ada1eda73b5e35891d0e99b77c5
Philip Brohan <philip@brohan.org>
2020-01-29 17:02:22 +0000

    Added ERA5-20CRv3 comparison

---
+++ comparisons/ERA5_20CRv3/E-ensemble_E-grid/make_comparison_slice.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+# Make an latitude slice comparing 20CRv3 and ERA5 - for a given month,
+#  sampling across longitude and ensemble.
+
+# Actually, do this for every month in a year - makes a more
+#  reasonable unit of work
+
+import os
+import iris
+import numpy
+import pickle
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--year", help="Year",
+                    type=int,required=True)
+parser.add_argument("--opdir", help="Directory for output files",
+                    default="%s/EUSTACE/derived/ERA5_20CRv3_comparison" % \
+                                           os.getenv('SCRATCH'),
+                    type=str,required=False)
+args = parser.parse_args()
+if not os.path.isdir(args.opdir):
+    os.makedirs(args.opdir)
+
+# Fix dask SPICE bug
+import dask
+dask.config.set(scheduler='single-threaded')
+
+# Get EUSTACE field for grid dimensions
+egrid = iris.load_cube(("%s/EUSTACE/1.0/1969/"+
+                        "tas_global_eustace_0_19690312.nc") % 
+                        os.getenv('SCRATCH'),
+            iris.Constraint(cube_func=(lambda c: c.var_name == 'tas')))
+
+# Make the 20CR climatology
+n=[]
+for m in range(1,13):
+    mc=iris.Constraint(time=lambda cell: cell.point.month == m and \
+                                         cell.point.year > 1980 and \
+                                         cell.point.year < 2011)
+    h=iris.load_cube('%s/20CR/version_3/monthly_means/air.2m.mon.mean.nc' % 
+                                                   os.getenv('SCRATCH'),
+                 iris.Constraint(name='air_temperature') & mc)
+    n.append(h.extract(mc).collapsed('time', iris.analysis.MEAN))
+
+# Make the ERA5 climatology
+n5=[]
+for m in range(1,13):
+    mc=iris.Constraint(time=lambda cell: cell.point.month == m and \
+                                         cell.point.year > (1981-1) and \
+                                         cell.point.year < (2010+1))
+    h=iris.load('%s/ERA5/monthly_averaged_ensemble_members/t2m.nc' % 
+                                                   os.getenv('SCRATCH'),
+                 iris.Constraint(name='2 metre temperature') & mc)[1]
+    n5.append(h.extract(mc).collapsed(['time','ensemble_member'],
+                                     iris.analysis.MEAN))
+
+
+for month in range(1,13):
+
+    # Array to store the sample in
+    ndata=numpy.ma.array(numpy.zeros((1,720)),mask=False)
+
+    # Load the 20CR data, anomalise, and regrid to EUSTACE grid
+    mc=iris.Constraint(time=lambda cell: cell.point.month == month and \
+                                         cell.point.year == args.year)
+    e=iris.load_cube('%s/20CR/version_3/monthly_means/air.2m.mon.mean.nc' % 
+                                                   os.getenv('SCRATCH'),
+                 iris.Constraint(name='air_temperature') & mc)
+    e = e - n[month-1]
+    e = e.regrid(egrid,iris.analysis.Nearest())
+
+    # Load the ERA5 data
+    h=[]
+    for member in range(10):
+        m = iris.load('%s/ERA5/monthly_averaged_ensemble_members/t2m.nc' %
+                                                           os.getenv('SCRATCH'),
+                          iris.Constraint(ensemble_member=member) & mc)[1]
+
+        m = m - n5[month-1]
+
+        m = m.regrid(egrid,iris.analysis.Nearest())
+        h.append(m)
+
+    # Make the slice
+    for lat in range(720):
+        m_h = numpy.random.randint(10)
+        rand_l = numpy.random.randint(0,1440)
+        ndata[0,lat]=h[m_h].data[lat,rand_l]-e.data[lat,rand_l]
+
+    # Store
+    dfile = "%s/%04d%02d.pkl" % (args.opdir,args.year,month)
+    pickle.dump( ndata, open( dfile, "wb" ) )
---
+++ comparisons/ERA5_20CRv3/E-ensemble_E-grid/plot_comparison_with_frame.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python
+
+# Compare pre-calculated stripes from 20CRv3 and ERA5
+# Add axes and a grid.
+
+import os
+import numpy
+import datetime
+import pickle
+
+import matplotlib
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+from matplotlib.lines import Line2D
+
+start=datetime.datetime(1851,1,1,0,0)
+end=datetime.datetime(2018,12,31,23,59)
+
+fig=Figure(figsize=(19.2,13),              # Width, Height (inches)
+           dpi=300,
+           facecolor=(0.5,0.5,0.5,1),
+           edgecolor=None,
+           linewidth=0.0,
+           frameon=False,                
+           subplotpars=None,
+           tight_layout=None)
+canvas=FigureCanvas(fig)
+matplotlib.rc('image',aspect='auto')
+
+def add_latline(ax,latitude):
+    latl = (latitude+90)/180
+    ax.add_line(Line2D([start.timestamp(),end.timestamp()], 
+                       [latl,latl], 
+                       linewidth=0.5, 
+                       color=(0.8,0.8,0.8,1),
+                       zorder=200))
+
+# HadCRUT5 at the top
+
+# Add a textured grey background
+s=(2000,600)
+ax2 = fig.add_axes([0,0.67+0.03,1,0.32],facecolor='green')
+ax2.set_axis_off() # Don't want surrounding x and y axis
+nd2=numpy.random.rand(s[1],s[0])
+clrs=[]
+for shade in numpy.linspace(.42+.01,.36+.01):
+    clrs.append((shade,shade,shade,1))
+y = numpy.linspace(0,1,s[1])
+x = numpy.linspace(0,1,s[0])
+img = ax2.pcolormesh(x,y,nd2,
+                        cmap=matplotlib.colors.ListedColormap(clrs),
+                        alpha=1.0,
+                        shading='gouraud',
+                        zorder=10)
+
+# Plot the stripes
+ax = fig.add_axes([0,0.67+0.03,1,0.32],facecolor='black',
+                  xlim=((start+datetime.timedelta(days=1)).timestamp(),
+                        (end-datetime.timedelta(days=1)).timestamp()),
+                  ylim=(0,1))
+ax.set_axis_off() 
+
+(ndata,dts) = pickle.load( open( "ERA5.pkl", "rb" ) )
+
+ndata = numpy.transpose(ndata)
+s=ndata.shape
+y = numpy.linspace(0,1,s[0]+1)
+x = [(a-datetime.timedelta(days=15)).timestamp() for a in dts]
+x.append((dts[-1]+datetime.timedelta(days=15)).timestamp())
+img = ax.pcolorfast(x,y,numpy.cbrt(ndata),
+                        cmap='RdYlBu_r',
+                        alpha=1.0,
+                        vmin=-1.7,
+                        vmax=1.7,
+                        zorder=100)
+
+for lat in [-60,-30,0,30,60]:
+    add_latline(ax,lat)
+
+# 20CRv3 in the middle
+
+# Add a textured grey background
+s=(2000,600)
+ax2 = fig.add_axes([0,0.335+0.03,1,0.32],facecolor='green')
+ax2.set_axis_off() # Don't want surrounding x and y axis
+nd2=numpy.random.rand(s[1],s[0])
+clrs=[]
+for shade in numpy.linspace(.42+.01,.36+.01):
+    clrs.append((shade,shade,shade,1))
+y = numpy.linspace(0,1,s[1])
+x = numpy.linspace(0,1,s[0])
+img = ax2.pcolormesh(x,y,nd2,
+                        cmap=matplotlib.colors.ListedColormap(clrs),
+                        alpha=1.0,
+                        shading='gouraud',
+                        zorder=10)
+
+# Plot the stripes
+ax = fig.add_axes([0,0.335+0.03,1,0.32],facecolor='black',
+                  xlim=((start+datetime.timedelta(days=1)).timestamp(),
+                        (end-datetime.timedelta(days=1)).timestamp()),
+                  ylim=(0,1))
+ax.set_axis_off() 
+
+(ndata,dts) = pickle.load( open( "20CRv3.pkl", "rb" ) )
+
+ndata = numpy.transpose(ndata)
+s=ndata.shape
+y = numpy.linspace(0,1,s[0]+1)
+x = [(a-datetime.timedelta(days=15)).timestamp() for a in dts]
+x.append((dts[-1]+datetime.timedelta(days=15)).timestamp())
+img = ax.pcolorfast(x,y,numpy.cbrt(ndata),
+                        cmap='RdYlBu_r',
+                        alpha=1.0,
+                        vmin=-1.7,
+                        vmax=1.7,
+                        zorder=100)
+
+for lat in [-60,-30,0,30,60]:
+    add_latline(ax,lat)
+
+# Difference at the bottom
+
+# Add a textured grey background
+s=(2000,600)
+ax2 = fig.add_axes([0,0.03,1,0.32],facecolor='green')
+ax2.set_axis_off() # Don't want surrounding x and y axis
+nd2=numpy.random.rand(s[1],s[0])
+clrs=[]
+for shade in numpy.linspace(.42+.01,.36+.01):
+    clrs.append((shade,shade,shade,1))
+y = numpy.linspace(0,1,s[1])
+x = numpy.linspace(0,1,s[0])
+img = ax2.pcolormesh(x,y,nd2,
+                        cmap=matplotlib.colors.ListedColormap(clrs),
+                        alpha=1.0,
+                        shading='gouraud',
+                        zorder=10)
+
+# Plot the stripes
+ax = fig.add_axes([0,0.03,1,0.32],facecolor='black',
+                  xlim=((start+datetime.timedelta(days=1)).timestamp(),
+                        (end-datetime.timedelta(days=1)).timestamp()),
+                  ylim=(0,1))
+ax.set_axis_off() 
+
+(ndata,dts) = pickle.load( open( "comparison.pkl", "rb" ) )
+
+ndata = numpy.transpose(ndata)
+s=ndata.shape
+y = numpy.linspace(0,1,s[0]+1)
+x = [(a-datetime.timedelta(days=15)).timestamp() for a in dts]
+x.append((dts[-1]+datetime.timedelta(days=15)).timestamp())
+img = ax.pcolorfast(x,y,numpy.cbrt(ndata),
+                        cmap='RdYlBu_r',
+                        alpha=1.0,
+                        vmin=-1.7,
+                        vmax=1.7,
+                        zorder=100)
+
+for lat in [-60,-30,0,30,60]:
+    add_latline(ax,lat)
+
+# Add a date grid
+axg = fig.add_axes([0,0,1,1],facecolor='green',
+                  xlim=((start+datetime.timedelta(days=1)).timestamp(),
+                        (end-datetime.timedelta(days=1)).timestamp()),
+                  ylim=(0,1))
+axg.set_axis_off()
+
+def add_dateline(ax,year):
+    x = datetime.datetime(year,1,1,0,0).timestamp()
+    ax.add_line(Line2D([x,x], [0.02,1.0], 
+                linewidth=0.5, 
+                color=(0.8,0.8,0.8,1),
+                       zorder=200))
+    ax.text(x,0.015,
+         "%04d" % year,
+         horizontalalignment='center',
+         verticalalignment='center',
+         color='black',
+         size=14,
+         clip_on=True,
+         zorder=200)
+
+for year in range(1860,2020,10):
+    add_dateline(axg,year)
+
+fig.savefig('comparison_grid.png')
