040e30f936b7b2b617c5517e45d73bf149f20a40
Philip Brohan <philip@brohan.org>
2023-07-13 13:36:55 +0100

    Fix a few cosmetic git issues.

---
+++ EUSTACE/make_normals/daily/makeall_sd.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# Scripts to make normals for every day
+
+import os
+import datetime
+
+startyear=1969  # Any non leap year is fine
+
+cdy = datetime.date(startyear,1,1)
+while cdy.year==startyear:
+    opf = "%s/EUSTACE/1.0/sd_1961_1990/%02d%02d.nc" %\
+                  (os.getenv('SCRATCH'),cdy.month,cdy.day)
+    if not os.path.isfile(opf):
+        print("./make_sd_for_day.py --month=%d --day=%d" %
+                (cdy.month,cdy.day))
+    cdy += datetime.timedelta(days=1)
