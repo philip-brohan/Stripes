506cfcc3b8e3f968d1b09d57ce3c4c91e355977a
Philip Brohan <philip@brohan.org>
2023-07-14 15:00:09 +0100

    Added scripts to plot a version with a colour-bar.

--- HadCRUT5/ensemble_zonal_mean/get_sample.py
+++ HadCRUT5/ensemble_zonal_mean/get_sample.py
@@ -2,18 +2,22 @@
 # Monthly, resolved in latitude, averaging in longitude, sampling the ensemble.
 
 import os
+import sys
 import iris
 import numpy
 import datetime
 
-def get_sample_cube(start=datetime.datetime(1851,1,1,0,0),
-                    end=datetime.datetime(2018,12,31,23,59),
-                    climatology=None,
-                    new_grid=None,rstate=None):
 
+def get_sample_cube(
+    start=datetime.datetime(1851, 1, 1, 0, 0),
+    end=datetime.datetime(2018, 12, 31, 23, 59),
+    climatology=None,
+    new_grid=None,
+    rstate=None,
+):
     # Choose ten ensemble members (Should be half odd and half even,
     #   and half from 1-50, half from 51-100)
-    members = (1,2,3,4,5,56,57,58,59,60)
+    members = (1, 2, 3, 4, 5, 56, 57, 58, 59, 60)
 
     # Might want the longitude random sampling to be reproducible
     if rstate is None:
@@ -24,36 +28,41 @@ def get_sample_cube(start=datetime.datetime(1851,1,1,0,0),
     r_ensemble = numpy.random.RandomState(seed=None)
 
     # Load the HadCRUT5 analysis data
-    h=[]
+    h = []
     for member in members:
-       m = iris.load_cube("/scratch/hadcc/hadcrut5/build/HadCRUT5/analysis/"+
-                     "HadCRUT.5.0.0.0.analysis.anomalies.%d.nc" % member,
-                     iris.Constraint(time=lambda cell: start <= cell.point <=end))
+        m = iris.load_cube(
+            "/data/users/hadobs/hadcrut5/HadCRUT.5.0.1.0-202305/"
+            + "hadcrut5/dev/build/HadCRUT5/analysis/"
+            + "HadCRUT.5.0.1.0.analysis.anomalies.%d.nc" % member,
+            iris.Constraint(time=lambda cell: start <= cell.point <= end),
+        )
 
-       # It's an anomaly dataset, but we still sometimes need a climatology
-       #  to re-base the anomalies to a different period.
-       if climatology is not None:
-           dts = m.coords('time')[0].units.num2date(m.coords('time')[0].points)
-           for tidx in range(len(dts)):
-               midx=dts[tidx].month-1
-               m.data[tidx,:,:] -= climatology[midx].data
+        # It's an anomaly dataset, but we still sometimes need a climatology
+        #  to re-base the anomalies to a different period.
+        if climatology is not None:
+            dts = m.coords("time")[0].units.num2date(m.coords("time")[0].points)
+            for tidx in range(len(dts)):
+                midx = dts[tidx].month - 1
+                m.data[tidx, :, :] -= climatology[midx].data
 
-       if new_grid is not None:
-           m = m.regrid(new_grid,iris.analysis.Nearest())
-       h.append(m)
+        if new_grid is not None:
+            m = m.regrid(new_grid, iris.analysis.Nearest())
 
-    dts = h[0].coords('time')[0].units.num2date(h[0].coords('time')[0].points)
+        h.append(m)
 
-    # Mean longitude at each month
-    s=h[0].data.shape
-    ndata=numpy.ma.array(numpy.zeros((s[0],s[1])),mask=True)
+    dts = h[0].coords("time")[0].units.num2date(h[0].coords("time")[0].points)
+
+    
+    
+
+    # Random member, and mean longitude, at each month
+    s = h[0].data.shape
+    ndata = numpy.ma.array(numpy.zeros((s[0], s[1])), mask=True)
     for t in range(s[0]):
-       for lat in range(s[1]):
-           member = r_ensemble.randint(len(members))
-           ndata[t,lat]=numpy.mean(h[member].data[t,lat,:])
-           if (numpy.ma.count_masked(h[member].data[t,lat,:]) > 
-                                      numpy.random.randint(s[2])):
-               ndata.mask[t,lat]=True
+        for lat in range(s[1]):
+            member = r_ensemble.randint(0,len(h))
+            ndata[t, lat] = numpy.mean(h[member].data[t, lat,:])
+            if numpy.ma.count_masked(h[member].data[t, lat,:]) > r_long.randint(0,s[2]):
+                ndata.mask[t, lat] = True
 
-    return (ndata,dts)
-
+    return (ndata, dts)
---
+++ HadCRUT5/ensemble_zonal_mean/stripes+colour_bar.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+
+# Make an extended climate-stripes image from HadCRUT5
+# Monthly, resolved in latitude, sampling in longitude, sampling the ensemble.
+# Adds a colour bar & latitude axis - for formal use.
+
+import os
+import sys
+import iris
+import numpy
+import datetime
+
+import matplotlib
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+from matplotlib.patches import Rectangle
+from matplotlib.lines import Line2D
+
+start = datetime.datetime(1851, 1, 1, 0, 0)
+end = datetime.datetime(2022, 12, 31, 23, 59)
+
+from get_sample import get_sample_cube
+
+(ndata, dts) = get_sample_cube(start, end)
+
+# Plot the resulting array as a 2d colourmap
+fig = Figure(
+    figsize=(16, 4.5),  # Width, Height (inches)
+    dpi=200,
+    facecolor=(0.5, 0.5, 0.5, 1),
+    edgecolor=None,
+    linewidth=0.0,
+    frameon=False,
+    subplotpars=None,
+    tight_layout=None,
+)
+canvas = FigureCanvas(fig)
+matplotlib.rc("image", aspect="auto")
+
+# White background
+axb = fig.add_axes(
+    [0.0, 0.0, 1.0, 1.0],
+    facecolor='white',
+    xmargin=0,ymargin=0,
+    )
+axb.set_axis_off()
+axb.fill([0,1,1,0],[0,0,1,1],'white')
+
+
+def add_latline(ax, latitude):
+    latl = (latitude + 90) / 180
+    ax.add_line(
+        Line2D(
+            [start.timestamp(), end.timestamp()],
+            [latl, latl],
+            linewidth=0.75,
+            color=(0.2, 0.2, 0.2, 1),
+            zorder=200,
+        )
+    )
+
+
+# Add a textured grey background
+s = (2000, 100)
+ax2 = fig.add_axes([0.0, 0.05, 0.9, 0.95], facecolor="green")
+ax2.set_axis_off()  # Don't want surrounding x and y axis
+nd2 = numpy.random.rand(s[1], s[0])
+clrs = []
+for shade in numpy.linspace(0.42 + 0.01, 0.36 + 0.01):
+    clrs.append((shade, shade, shade, 1))
+y = numpy.linspace(0, 1, s[1])
+x = numpy.linspace(0, 1, s[0])
+img = ax2.pcolormesh(
+    x,
+    y,
+    nd2,
+    cmap=matplotlib.colors.ListedColormap(clrs),
+    alpha=1.0,
+    shading="gouraud",
+    zorder=10,
+)
+
+# Plot the stripes
+ax = fig.add_axes(
+    [0.0, 0.05, 0.9, 0.95],
+    facecolor="black",
+    xlim=(
+        (start + datetime.timedelta(days=1)).timestamp(),
+        (end - datetime.timedelta(days=1)).timestamp(),
+    ),
+    ylim=(0, 1),
+)
+ax.set_axis_off()
+
+ndata = numpy.transpose(ndata)
+s = ndata.shape
+y = numpy.linspace(0, 1, s[0] + 1)
+x = [datetime.datetime.fromisoformat((a - datetime.timedelta(days=15)).isoformat()).timestamp() for a in dts]
+x.append(datetime.datetime.fromisoformat((dts[-1] + datetime.timedelta(days=15)).isoformat()).timestamp())
+img = ax.pcolorfast(
+    x, y, numpy.cbrt(ndata), cmap="RdYlBu_r", alpha=1.0, vmin=-2.0, vmax=2.0, zorder=100
+)
+
+# Add a latitude grid
+for lat in [-60, -30, 0, 30, 60]:
+    add_latline(ax, lat)
+
+# Add a date grid
+axg = fig.add_axes(
+    [0.0, 0, 0.9, 1],
+    facecolor="green",
+    xlim=(
+        (start + datetime.timedelta(days=1)).timestamp(),
+        (end - datetime.timedelta(days=1)).timestamp(),
+    ),
+    ylim=(0, 1),
+)
+axg.set_axis_off()
+
+
+def add_dateline(ax, year):
+    x = datetime.datetime(year, 1, 1, 0, 0).timestamp()
+    ax.add_line(
+        Line2D(
+            [x, x], [0.04, 1.0], linewidth=0.75, color=(0.2, 0.2, 0.2, 1), zorder=200
+        )
+    )
+    ax.text(
+        x,
+        0.025,
+        "%04d" % year,
+        horizontalalignment="center",
+        verticalalignment="center",
+        color="black",
+        size=12,
+        clip_on=True,
+        zorder=200,
+    )
+
+
+for year in range(1860, 2023, 10):
+    add_dateline(axg, year)
+
+# ColourBar
+ax_cb = fig.add_axes([0.925, 0.06125, 0.05, 0.9])
+ax_cb.set_axis_off()
+cb = fig.colorbar(
+    img, ax=ax_cb, location="right", orientation="vertical", fraction=1.0, label='Anomaly (\N{degree sign}C)'
+)
+
+
+fig.savefig("HadCRUT5+cb.png")
